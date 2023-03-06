a9c904b6a54c9679b6a626c48811c127763ebc53
vaniamaksimov <vaniamaksimov@gmail.com>
2023-03-06 07:56:58 +0300

    add tests for login page and main page

---
+++ tests/test_login_page.py
@@ -0,0 +1,10 @@
+from pages import LoginPage
+
+
+def test_guest_should_see_forms(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
